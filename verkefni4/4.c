2d4c80d2182151b24ee8c9539dbb2d7ca8a7d0b8
Neptúnus Egilsson <neptunus@hirt.is>
2018-03-02 09:32:38 +0000

    assignment 4
    updated - backs away when it can't go forward, checks every two seconds

--- verkefni4/4.c
+++ verkefni4/4.c
@@ -16,12 +16,14 @@
 Neptunus / Isar
 
 Robot turns successfully away from obstacles when they are right in front of it
-but it has trouble when approaching walls from acute angles.
+but it has trouble when approaching walls from sharp angles.
 I'm considering a solution that involves checking the encoders when the robot is
 attempting to drive forward with some frequency, and if their value hasn't
 changed by a significant amount then the robot starts a sequence of backing up
 and then turning left.
 */
+int sonar_delta;
+int sonar_value;
 
 void stop(){
 	motor[rightMotor] = motor[leftMotor] = 0;
@@ -34,8 +36,12 @@ void drive(int right_speed = 127, int left_speed = 127) {
 
 task solve() {
 	int speed;
-	int sonar_value;
+	sonar_value = SensorValue(sonarSensor);
+	sonar_delta = sonar_value;
+
 	int distance = 50;
+
+	ClearTimer(T1);
 	while(true) {
 		sonar_value = SensorValue(sonarSensor);
 		speed = 50;
@@ -46,12 +52,33 @@ task solve() {
 			wait10Msec(100);
 			drive(speed, -speed);
 			wait10Msec(200);
+
+			ClearTimer(T1);
 		}
+		if (time1[T1] > 2000) {
+			if (abs(sonar_value - sonar_delta) < 5) {
+				stop();
+				wait10Msec(100);
+				drive(-speed, -speed);
+				wait10Msec(150);
+				drive(speed, -speed);
+				wait10Msec(200);
+				stop();
+			}
+			sonar_delta = sonar_value;
+			ClearTimer(T1);
+		}
+
 
 		clearLCDLine(0);
     displayLCDPos(0,0);
     displayNextLCDString("Sonar: ");
-    displayNextLCDNumber(SensorValue(sonarSensor));
+    displayNextLCDNumber(sonar_value);
+
+    clearLCDLine(1);
+    displayLCDPos(1,0);
+    displayNextLCDString("Delta: ");
+    displayNextLCDNumber(sonar_delta);
 	}
 }
 
